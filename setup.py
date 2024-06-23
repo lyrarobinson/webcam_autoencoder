c402c4b9cc3424bd14430661334111817a7bdf8f
lyrarobinson <alexrobinson12345678@gmail.com>
2024-06-24 00:46:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='webcam_autoencoder',
     version='0.1',
+    py_modules=['main', 'makegrids'],  # List all top-level modules here
     install_requires=[
-		'pygame==2.5.2',
-		'numpy==1.26.4',
-		'tensorflow==2.16.1',
-		'keras==3.3.3',
-		'matplotlib==3.9.0',
-		'pillow==10.3.0',
-		'opencv-python==4.10.0.82'
+        'pygame==2.5.2',
+        'numpy==1.26.4',
+        'tensorflow==2.16.1',
+        'keras==3.3.3',
+        'matplotlib==3.9.0',
+        'pillow==10.3.0',
+        'opencv-python==4.10.0.82'
     ],
-	entry_points={
-		'console_scripts': [
-			'webcam_autoencoder=main:main',
-		],
-	},
+    entry_points={
+        'console_scripts': [
+            'webcam_autoencoder=main:main',
+        ],
+    },
     python_requires='>=3.9',
 )
